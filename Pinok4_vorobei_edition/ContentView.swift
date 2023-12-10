97d9e9e5eb47602e72c6e541746a2d0f7e318f8b
exhanter <2ivan.tkachev@gmail.com>
2023-12-10 14:45:54 +0100

    with continuous animation

--- Pinok4_vorobei_edition/ContentView.swift
+++ Pinok4_vorobei_edition/ContentView.swift
@@ -6,43 +6,32 @@
 //
 
 import SwiftUI
-import Observation
 
 struct AnimatedButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding(10)
-            .background(configuration.isPressed ? Color(white: 0.9) : .white)
+            .background(configuration.isPressed ? Color(white: 0.5) : .white)
             .foregroundStyle(.black)
             .clipShape(.circle)
-            .scaleEffect(configuration.isPressed ? 0.7 : 1)
-            .animation(.spring, value: configuration.isPressed)
+            .animation(.linear(duration: 0.22), value: configuration.isPressed)
     }
 }
-struct AnimatedButton2: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding(10)
-            .background(configuration.isPressed ? Color(white: 0.9) : .white)
-            .foregroundStyle(.black)
-            .clipShape(.circle)
-            .scaleEffect(configuration.isPressed ? 0.7 : 1)
-
-    }
-}
-
 struct ContentView: View {
-    
+    @State private var data = Data()
     @State private var performAnimation = false
     @State private var performAnimation2 = false
     
     var body: some View {
-        HStack(spacing: 100) {
             Button {
                 if !performAnimation {
                     withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
                         performAnimation = true
+                        performAnimation2 = true
                     } completion: {
+                        withAnimation {
+                            performAnimation2 = false
+                        }
                         performAnimation = false
                     }
                 }
@@ -50,7 +39,6 @@ struct ContentView: View {
                 GeometryReader { proxy in
                     let width = proxy.size.width / 2
                     let systemName = "play.fill"
-                    
                     HStack(alignment: .center, spacing: 0) {
                         Image(systemName: systemName)
                             .renderingMode(.template)
@@ -71,51 +59,11 @@ struct ContentView: View {
                             .opacity(performAnimation ? .zero : 1)
                     }
                     .frame(maxHeight: .infinity, alignment: .center)
-                    
                 }
             }
             .frame(maxWidth: 62, maxHeight: 62)
             .buttonStyle(AnimatedButton())
-            
-            Button {
-                if !performAnimation2 {
-                    withAnimation(.interpolatingSpring(stiffness: 170, damping: 15)) {
-                        performAnimation2 = true
-                    } completion: {
-                        performAnimation2 = false
-                    }
-                }
-            } label: {
-                GeometryReader { proxy in
-                    let width = proxy.size.width / 2
-                    let systemName = "play.fill"
-                    
-                    HStack(alignment: .center, spacing: 0) {
-                        Image(systemName: systemName)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: performAnimation2 ? width : .zero)
-                            .opacity(performAnimation2 ? 1 : .zero)
-                        Image(systemName: systemName)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: width)
-                        Image(systemName: systemName)
-                            .renderingMode(.template)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: performAnimation2 ? 0.5 : width)
-                            .opacity(performAnimation2 ? .zero : 1)
-                    }
-                    .frame(maxHeight: .infinity, alignment: .center)
-                    
-                }
-            }
-            .frame(maxWidth: 62, maxHeight: 62)
-            .buttonStyle(AnimatedButton2())
-        }
+            .scaleEffect(performAnimation2 ? 0.86 : 1)
         }
 }
 
